325a9b6a9e8dd265ca69a9ddf3bee40145cef136
Daniel Jonasson <daniel@intesys.it>
2013-04-04 17:14:00 +0200

    cleanup

--- lib/fooltip/active_record.rb
+++ lib/fooltip/active_record.rb
@@ -1,9 +1,7 @@
 module Fooltip
   module ActiveRecord
 
-    def has_fooltips(options = {})
-      #options.assert_valid_keys(:x)
-
+    def has_fooltips
       attr_accessible :fooltip_ids
       has_many :fooltip_associations, as: :owner, class_name: "Fooltip::Association", dependent: :destroy
       has_many :fooltips, through: :fooltip_associations, source: :container, class_name: "Fooltip::Container"
