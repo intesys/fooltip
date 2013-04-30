8314d6d6321287d6d472f80c0d1f75ba5833387b
Daniel Jonasson <daniel@intesys.it>
2013-04-30 09:37:43 +0200

    make sure we accept nested attributes for fooltip associations

--- lib/fooltip/active_record.rb
+++ lib/fooltip/active_record.rb
@@ -2,9 +2,10 @@ module Fooltip
   module ActiveRecord
 
     def has_fooltips
-      attr_accessible :fooltip_ids
+      attr_accessible :fooltip_associations_attributes
       has_many :fooltip_associations, as: :owner, class_name: "Fooltip::Association", dependent: :destroy
       has_many :fooltips, through: :fooltip_associations, source: :container, class_name: "Fooltip::Container"
+      accepts_nested_attributes_for :fooltip_associations, reject_if: :all_blank, allow_destroy: true
     end
 
   end
